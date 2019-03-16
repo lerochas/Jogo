6243169cb28d8bce4a6e6247c1d4c5255601a346
Leandro Rocha <le_3693@hotmail.com>
2019-03-16 16:18:54 -0300

    estrutura do projeto com api e leitura de arquivo

--- game-quake/src/main/java/com/game/gamequake/entity/Game.java
+++ game-quake/src/main/java/com/game/gamequake/entity/Game.java
@@ -1,6 +1,7 @@
 package com.game.gamequake.entity;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Map;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
@@ -12,6 +13,12 @@ public class Game implements Serializable{
 	private String[] players;
 	private Map<String,Integer> kills;
 	
+	public Game(String string, String phraseList) {
+		// TODO Auto-generated constructor stub
+	}
+	public Game() {
+		// TODO Auto-generated constructor stub
+	}
 	public Integer getTotalKills() {
 		return totalKills;
 	}
@@ -29,5 +36,29 @@ public class Game implements Serializable{
 	}
 	public void setKills(Map<String, Integer> kills) {
 		this.kills = kills;
+	}
+	public String getPhraseList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public void setPhraseList(ArrayList arrayList) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setAction(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setWho(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setPlayer(String trim) {
+		// TODO Auto-generated method stub
+		
+	}
+	public void setCause(String substring) {
+		// TODO Auto-generated method stub
+		
 	}
 }
--- game-quake/src/main/java/com/game/gamequake/service/ParserService.java
+++ game-quake/src/main/java/com/game/gamequake/service/ParserService.java
@@ -1,15 +1,96 @@
 package com.game.gamequake.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.stereotype.Service;
 
 import com.game.gamequake.entity.Game;
 
+
 @Service
 public class ParserService {
-	public List<Game> parseGame(List<String> game){
+	
+	private static final String KILL = "Kill";
+    private static final String KILLED = "killed";
+    private static final String LINE_EMPTY     = "------------------------------------------------------------";
+
+    public List<Game> getGame(final List<String> lines) {
+        List<Game> games = new ArrayList<>();
+        Game game = null;
+        for (String line: lines) {
+            if(LINE_EMPTY.equals(parserLineToGame(line))) {
+                if(game != null) {
+                    if(!game.getPhraseList().isEmpty()) {
+                        games.add(new Game("game_"+(games.size()+1), game.getPhraseList()));
+                    }
+                }
+                game = new Game() ;
+                game.setPhraseList(new ArrayList<>());
+            }
+            try {
+                Game gamep = parserLineToGameP(line);
+                games.add(gamep);
+            } catch (Exception e) {
+                System.out.print(e.getMessage());            
+                }
+        }
+        return games;
+    }
+
+    private Game parserLineToGameP(String line) {
+		// TODO Auto-generated method stub
 		return null;
 	}
+
+	private String parserLineToGame(String line) {
+        String[] splitLine = line.split(":");
+
+        if(splitLine.length >= 2 && splitLine[1].contains(LINE_EMPTY)) {
+            return LINE_EMPTY;
+        }
+
+        return null;
+    }
+
+    private Game parserLineToPhrase(String line){
+        String[] splitLine = line.split(":");
+        final Game phrase = new Game();
+
+        if(Pattern.matches("(?s).*killed.*", line)){
+        	findPhrase(splitLine[3], phrase);
+            return phrase;
+        } else {
+            throw new UnprocessableLineException();
+        }
+    }
+
+    private void findPlayer(final String line, final Game phrase) {
+        if(line.contains("<world>")) {
+            phrase.setAction("killed");
+            phrase.setWho("<world>");
+            phrase.setPlayer(line.substring(getBeginPlayerIndex(line, KILLED.length()), line.indexOf("by")).trim());
+        } else {
+            phrase.setAction("kill");
+            phrase.setWho(line.substring(getBeginPlayerIndex(line, KILLED.length()), line.indexOf("by")).trim());
+            phrase.setPlayer(line.substring(0, getBeginPlayerIndex(line, 0)).trim());
+        }
+    }
+
+    private void findPhrase(String line, Game phrase) {
+        findPlayer(line, phrase);
+        findMOD(line, phrase);
+    }
+
+    private void findMOD(final String line, final Game phrase) {
+        phrase.setCause(line.substring(line.indexOf("MOD")));
+    }
+
+    private int getBeginPlayerIndex(final String line, final int plus) {
+        return line.indexOf(KILLED)+plus;
+    }
+
 	
 }
