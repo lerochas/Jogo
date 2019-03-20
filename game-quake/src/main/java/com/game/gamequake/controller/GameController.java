bc9a4a9320106657fbd9384d6ecf55a3b8e2b54e
Leandro Rocha <le_3693@hotmail.com>
2019-03-19 22:14:41 -0300

    criando método em GameController

--- game-quake/src/main/java/com/game/gamequake/controller/GameController.java
+++ game-quake/src/main/java/com/game/gamequake/controller/GameController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -26,5 +27,17 @@ public class GameController {
             log.error("Erro GameController: ", e);
             return "Erro: ";
 		}
+    }
+
+    @GetMapping(path= "/games/{id}", produces= MediaType.APPLICATION_JSON_VALUE)
+    public String only(@PathVariable String id) {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(returnGames.retornarJogos().get("jogo_"+id));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            log.error("Erro GameController: ", e);
+            return "Erro: ";
+        }
     }
 }
